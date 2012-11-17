90fc6ac6f3e608d807ebd7d473fe3add98149bae
Andrew Taylor <andrew@utterlyforked.co.uk>
2012-11-17 10:41:03 +0000

    Initial commit

---
+++ src/Example/Smtp.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Example;
+
+class Smtp implements \SplObserver {
+	
+	public function update(\SplSubject $subject){
+
+		$bodyTxt = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam purus nibh, volutpat a tempus quis, rhoncus id mauris. Aenean at nisi mauris. Quisque id interdum ipsum. Vestibulum eu nisi vel elit consequat cursus. Etiam volutpat urna ac justo iaculis iaculis. Nunc sed ligula ligula. Praesent in semper tortor. Donec feugiat laoreet massa vel congue. Vestibulum hendrerit odio eget purus semper laoreet. Maecenas dictum sem non ligula vehicula bibendum cursus enim scelerisque. Phasellus auctor lacus justo, pharetra auctor lorem. Sed elementum nulla non est luctus sit amet sodales turpis tempor. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Ut bibendum libero non orci vehicula condimentum. Nam ornare adipiscing pharetra.';
+
+	   	$headers = array("From"=>"apache@vm", "Subject"=>'[SMTP] Test Mail '.uniqid());
+	
+		$mail = \Mail::factory("smtp");
+	    	$mail->send("andrew@vm", $headers, $bodyTxt);
+  	}
+}
